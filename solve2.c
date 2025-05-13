f9a9b0b14031c54a1cbb535e151f9b2453547ac2
Istiaqe-Mahamud <istiaque.mahamud@gmail.com>
2025-05-13 23:40:07 +0600

    C HackerRank Solve

---
+++ solve2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    char str[100];
+    char sen[100];
+
+    scanf("%c", &ch);
+    getchar(); 
+
+    scanf("%s", str);
+    getchar();
+
+
+    fgets(sen, sizeof(sen), stdin);
+
+    printf("%c\n", ch);
+    printf("%s\n", str);
+    printf("%s", sen);
+
+    return 0;
+}
